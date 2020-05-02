c2eae2dcf405d726dd4121bc1ea9cce17d546b2d
Imad Razzak <im.razzak@gmail.com>
2020-05-02 19:03:04 +0200

    removed gtag

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import VueCompositionApi from '@vue/composition-api';
-import VueGtag from 'vue-gtag';
 
 Vue.use(VueCompositionApi);
 
@@ -9,24 +8,6 @@ Vue.config.productionTip = false;
 import './registerServiceWorker'
 const App = require('./App.vue').default;
 const router = require('./router').default;
-
-if (!window.webpackHotUpdate) {
-  const gtagId = 'UA-164225754-1';
-  Vue.use(VueGtag, {
-    config: {
-      id: gtagId,
-      params: {
-        send_page_view: false
-      },
-    },
-    onReady(gtag) {
-      // send initial page-view after load
-      gtag('config', gtagId, {
-        'page_path': location.hash.substring(1)
-      });
-    }
-  }, router);
-}
 
 new Vue({
   router,
