394ea4ee67afb3151ab69ceedcbcb1cb55e99f84
Imad Razzak <im.razzak@gmail.com>
2020-05-11 20:02:57 +0200

    cleanup

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   publicPath: process.env.VUE_APP_BASE_URL,
   lintOnSave: false,
   pwa: {
-    name: 'Total War: Warhammer - Interactive Map',
+    name: 'Total War: Warhammer II - Interactive Map',
     short_name: 'twwim',
     themeColor: '#000000',
     workboxPluginMode: 'InjectManifest',
